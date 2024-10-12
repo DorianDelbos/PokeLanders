cd8e49161dc4b14fb73cf40ef433d66f61700f78
Dorian <thesothief@gmail.com>
2024-10-12 20:25:29 +0200

    Assets and scenes
    Add all fighting assets
    Add fighting scene
    Update pc scene
    UI fighting

---
+++ PokeLanders/Assets/PokeLandersAssets/Codes/Landers/LanderDataNFC.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LanderDataNFC : ScriptableObject
+{
+    public ushort id = 0;
+    public string customName = string.Empty;
+    public ushort currentHp = 0;
+    public ushort currentXp = 0;
+    public ushort height = 0;
+    public ushort weight = 0;
+}
